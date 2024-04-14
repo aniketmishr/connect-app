c766db137226263156ac416861ca762cd7960e3b
aniketmishr <aniketmishra3476@gmail.com>
2024-04-14 14:11:24 +0530

    remove redundant annotations

--- app/src/main/java/com/example/connect/MainActivity.kt
+++ app/src/main/java/com/example/connect/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,7 +13,6 @@ import com.example.connect.ui.navigation.ConnectNavGraph
 import com.example.connect.ui.theme.ConnectTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalAnimationApi::class)
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
